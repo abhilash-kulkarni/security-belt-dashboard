5f50bf8d48fccb15b801d3ea03d57e74ac962091
abhilash kulkarni <abhilash.kulkarni@flipkart.com>
2016-05-18 13:05:59 +0530

    added models

---
+++ pi_app/models.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Belt(models.Model):
+	color = models.CharField(max_length = 10)
+	def __unicode__(self):
+		return u'%s' % self.color
+
+class Employee(models.Model):
+	employeeId = models.CharField(max_length = 6, unique = True)
+	employeeName = models.CharField(max_length = 50)
+	level = models.IntegerField(default = 1)
+	belt = models.ForeignKey(Belt)
+	def __unicode__(self):
+		return u'%s %s %s' % (self.employeeId, self.employeeName, self.level)
